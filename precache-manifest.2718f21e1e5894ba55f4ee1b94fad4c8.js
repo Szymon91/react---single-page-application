be70e53822d3b7c0cebb25351f68f7071bf21f99
Szymon91 <Szymon91@poczta.onet.pl>
2020-04-30 21:07:15 +0200

    Updates

---
+++ precache-manifest.2718f21e1e5894ba55f4ee1b94fad4c8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "458596fb6f0873e1af18c069c763e9c1",
+    "url": "/react---single-page-application/index.html"
+  },
+  {
+    "revision": "6961d887789dcbca12ed",
+    "url": "/react---single-page-application/static/css/main.1223739f.chunk.css"
+  },
+  {
+    "revision": "b1498ae3cd9a2af83d59",
+    "url": "/react---single-page-application/static/js/2.f133ec57.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react---single-page-application/static/js/2.f133ec57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6961d887789dcbca12ed",
+    "url": "/react---single-page-application/static/js/main.c8ca5f21.chunk.js"
+  },
+  {
+    "revision": "96db3168909f59862f6a",
+    "url": "/react---single-page-application/static/js/runtime-main.5985eb6a.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react---single-page-application/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react---single-page-application/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react---single-page-application/static/media/header3.d9d36b65.jpg"
+  }
+]);
